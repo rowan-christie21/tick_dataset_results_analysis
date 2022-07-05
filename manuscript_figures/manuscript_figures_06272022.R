0d771aad56a0b82d7195b805aa4519a9f0fb78ef
SMCCoder <sofie.christie@gmail.com>
2022-07-04 17:41:39 -0700

    added code for figures 6 and 7

--- manuscript_figures/manuscript_figures_06272022.R
+++ manuscript_figures/manuscript_figures_06272022.R
@@ -3827,3 +3827,317 @@ dev.off()
 
 ##############################
 
+county <- subset(tick_dataset_results, geographic_scope == "County")
+town <- subset(tick_dataset_results, geographic_scope == "Town")
+state_forest <- subset(tick_dataset_results, geographic_scope == "State forest")
+grid <- subset(tick_dataset_results, geographic_scope == "Grid")
+
+length(county$stability_time)
+#73 datasets
+length(town$stability_time)
+#186 datasets
+length(state_forest$stability_time)
+#6 datasets
+length(grid$stability_time)
+#24 datasets
+
+median(county$stability_time)
+#73 
+median(town$stability_time)
+#186 
+median(state_forest$stability_time)
+#6 
+median(grid$stability_time)
+#24 
+
+t.test(county$stability_time, town$stability_time)
+#t = -17.029, df = 243.09, p-value < 2.2e-16
+#signficiant
+
+t.test(county$stability_time, state_forest$stability_time)
+#t = -5.5457, df = 5.2241, p-value = 0.002278
+#significant
+
+t.test(county$stability_time, grid$stability_time)
+#t = -17.207, df = 33.87, p-value < 2.2e-16
+#signifcant
+
+t.test(town$stability_time, state_forest$stability_time)
+#t = 0.47098, df = 6.1169, p-value = 0.654
+#insignificant
+
+t.test(town$stability_time, grid$stability_time)
+#t = 0.22406, df = 86.176, p-value = 0.8232
+#insignificant
+
+t.test(state_forest$stability_time, grid$stability_time)
+#t = -0.37552, df = 6.0307, p-value = 0.7201
+#insignificant
+
+#----
+
+t.test(county$proportion_wrong_before_stability, town$proportion_wrong_before_stability)
+#t = 0.71555, df = 115.03, p-value = 0.4757
+#insignficiant
+
+t.test(county$proportion_wrong_before_stability, state_forest$proportion_wrong_before_stability)
+#t = 5.2409, df = 74.293, p-value = 1.445e-06
+#significant
+
+t.test(county$proportion_wrong_before_stability, grid$proportion_wrong_before_stability)
+#t = 1.3299, df = 43.066, p-value = 0.1905
+#insignifcant
+
+t.test(town$proportion_wrong_before_stability, state_forest$proportion_wrong_before_stability)
+#t = 7.4221, df = 46.441, p-value = 2.013e-09
+#significant
+
+t.test(town$proportion_wrong_before_stability, grid$proportion_wrong_before_stability)
+#t = 1.0046, df = 28.519, p-value = 0.3235
+#insignificant
+
+t.test(state_forest$proportion_wrong_before_stability, grid$proportion_wrong_before_stability)
+#t = -1.9093, df = 25.101, p-value = 0.06772
+#insignificant
+
+###############
+#geographic scope vs stability time
+###############
+
+#order factors
+tick_dataset_results$geographic_scope <- factor(tick_dataset_results$geographic_scope, c("Grid", "State forest", "Town", "County"))
+
+#set up compact letter display
+box.rslt <- with(tick_dataset_results, graphics::boxplot(stability_time ~ geographic_scope, plot = FALSE))
+ttest.rslt <- with(tick_dataset_results, pairwise.t.test(stability_time, geographic_scope, pool.sd = FALSE))
+ltrs <- make_letter_assignments(ttest.rslt)
+x <- c(1:length(ltrs$Letters))
+y <- box.rslt$stats[5, ]
+cbd <- ltrs$Letters
+ltr_df <- data.frame(x, y, cbd)
+
+# create boxplot for proportion significantly wrong between different geographic scopes
+stability_time_by_geographic_scope <- ggplot(tick_dataset_results, aes(x = geographic_scope, y = stability_time)) +
+  geom_boxplot() + 
+  geom_jitter() +
+  geom_text(data = ltr_df, aes(x=x, y=y, label=cbd), nudge_y = 1.25,color="red", size=6) +
+  scale_x_discrete(name = "Geographic scope") +
+  scale_y_continuous(name = "Stability time", limits = c(0,27), breaks = c(0,5,10,15,20,25)) +
+  theme(axis.line.x = element_line(size = 0.5, colour = "black"),
+        axis.line.y = element_line(size = 0.5, colour = "black"),
+        axis.line = element_line(size=1, colour = "black"),
+        panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(),
+        panel.border = element_blank(),
+        panel.background = element_blank(),
+        plot.title=element_text(size = 27, margin=margin(0,0,15,0)),
+        axis.text.x=element_text(colour="black", size = 18),
+        axis.text.y=element_text(colour="black", size = 18),
+        axis.title.x = element_text(size = 23, margin=margin(15,0,0,0)),
+        axis.title.y = element_text(size = 23, margin=margin(0,15,0,0)))
+
+stability_time_by_geographic_scope
+
+png(filename = paste("D:/Ixodes_scapularis_research_2019/tick_dataset_results_analysis/manuscript_figures/figure_6A_stability_time_by_geographic_scope ",Sys.Date(),".png", sep = ''), width = 600, height = 454)
+stability_time_by_geographic_scope
+dev.off()
+
+###############
+#geographic scope vs proportion wrong before stability
+###############
+
+# create boxplot for proportion significantly wrong between different geographic scopes
+tick_dataset_results$geographic_scope <- factor(tick_dataset_results$geographic_scope, c("Grid", "State forest", "Town", "County"))
+
+#set up compact letter display
+box.rslt <- with(tick_dataset_results, graphics::boxplot(proportion_wrong_before_stability ~ geographic_scope, plot = FALSE))
+ttest.rslt <- with(tick_dataset_results, pairwise.t.test(proportion_wrong_before_stability, geographic_scope, pool.sd = FALSE))
+ltrs <- make_letter_assignments(ttest.rslt)
+x <- c(1:length(ltrs$Letters))
+y <- box.rslt$stats[5, ]
+cbd <- ltrs$Letters
+ltr_df <- data.frame(x, y, cbd)
+
+proportion_wrong_before_stab_by_geographic_scope <- ggplot(tick_dataset_results, aes(x = geographic_scope, y = proportion_wrong_before_stability)) +
+  geom_boxplot() + 
+  geom_jitter() +
+  geom_text(data = ltr_df, aes(x=x, y=y, label=cbd), nudge_y = 0.05,color="red", size=6) +
+  scale_x_discrete(name = "Geographic scope") +
+  scale_y_continuous(name = "Proportion significantly wrong \nbefore stability", limits = c(0,1.06)) +
+  theme(axis.line.x = element_line(size = 0.5, colour = "black"),
+        axis.line.y = element_line(size = 0.5, colour = "black"),
+        axis.line = element_line(size=1, colour = "black"),
+        panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(),
+        panel.border = element_blank(),
+        panel.background = element_blank(),
+        plot.title=element_text(size = 27, margin=margin(0,0,15,0)),
+        axis.text.x=element_text(colour="black", size = 18),
+        axis.text.y=element_text(colour="black", size = 18),
+        axis.title.x = element_text(size = 23, margin=margin(15,0,0,0)),
+        axis.title.y = element_text(size = 23, margin=margin(0,15,0,0)))
+
+proportion_wrong_before_stab_by_geographic_scope
+
+png(filename = paste("D:/Ixodes_scapularis_research_2019/tick_dataset_results_analysis/manuscript_figures/figure_6B_proportion_wrong_before_stab_by_geographic_scope ",Sys.Date(),".png", sep = ''), width = 600, height = 454)
+proportion_wrong_before_stab_by_geographic_scope
+dev.off()
+
+###############
+# Combined plots 6A and 6B
+###############
+
+#arrange plots 6A and 6B into single image
+figure6AB <- ggarrange(
+  stability_time_by_geographic_scope + 
+    scale_x_discrete(name = NULL) + 
+    theme(axis.title.y = element_text(margin=margin(0,-20,0,0))), 
+  proportion_wrong_before_stab_by_geographic_scope, 
+  labels = c("A", "B"), nrow = 2, ncol=1, align = "v", font.label = list(size=25), hjust=-7
+)
+
+figure6AB
+
+png(filename = paste("D:/Ixodes_scapularis_research_2019/tick_dataset_results_analysis/manuscript_figures/figure_6AB ",Sys.Date(),".png", sep = ''), width = 600, height = 908)
+figure6AB
+dev.off()
+
+
+##############################
+
+# Figure 7A, 7B comparing stability time and proportion significantly wrong for sampling metric
+
+##############################
+
+pathogen <- subset(tick_dataset_results, tested_for_b_burgdoferi == "Y")
+abundance <- subset(tick_dataset_results, tested_for_b_burgdoferi == "N")
+
+length(pathogen$stability_time)
+#114 datasets
+length(abundance$stability_time)
+#175 datasets
+
+t.test(pathogen$stability_time, abundance$stability_time)
+#t = -1.2879, df = 283.9, p-value = 0.1988
+
+t.test(pathogen$proportion_wrong_before_stability, abundance$proportion_wrong_before_stability)
+#t = -1.1828, df = 232.98, p-value = 0.2381
+
+###############
+#ticks infected vs stability time
+###############
+
+tested_for_path <- tick_dataset_results[tick_dataset_results$tested_for_b_burgdoferi == "Y",] 
+tested_for_path$label <- "Tested for infection \nof B. burgdorferi"
+
+test_abuance <- tick_dataset_results[tick_dataset_results$tested_for_b_burgdoferi == "N",] 
+test_abuance$label <- "Sampled for \nAbundance"
+
+abundance_vs_infected_ticks <- c(tested_for_path$label, test_abuance$label)
+stability_time_for_abudance_vs_infected_ticks <- c(tested_for_path$stability_time, test_abuance$stability_time)
+
+tick_infection_data <- data.frame(abundance_vs_infected_ticks, stability_time_for_abudance_vs_infected_ticks)
+
+t.test(tick_infection_data[tick_infection_data$abundance_vs_infected_ticks == "Tested for infection \nof B. burgdorferi",]$stability_time_for_abudance_vs_infected_ticks, tick_infection_data[tick_infection_data$abundance_vs_infected_ticks == "Sampled for \nAbundance",]$stability_time_for_abudance_vs_infected_ticks)
+# t = -1.2879, df = 283.9, p-value = 0.1988
+# t-value indicates probability below 0.5
+# low probability of difference between datasets
+#
+# p-value higher than 0.05, accept null hypothesis
+# therefore alternative hypothesis: true difference in means is not equal to 0 is not supported
+# statiscally insignificant
+# no need for letter assignment
+
+#proportion significant by ticks infected and total ticks
+stability_time_by_metric <- ggplot(tick_infection_data, aes(x = abundance_vs_infected_ticks, y = stability_time_for_abudance_vs_infected_ticks)) +
+  geom_boxplot() + 
+  geom_jitter() +
+  scale_x_discrete(name = "Sampling metric") +
+  scale_y_continuous(name = "Stability time", limits = c(0, 25)) +
+  theme(axis.line.x = element_line(size = 0.5, colour = "black"),
+        axis.line.y = element_line(size = 0.5, colour = "black"),
+        axis.line = element_line(size=1, colour = "black"),
+        panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(),
+        panel.border = element_blank(),
+        panel.background = element_blank(),
+        plot.title=element_text(size = 27, margin=margin(0,0,15,0)),
+        axis.text.x=element_text(colour="black", size = 18),
+        axis.text.y=element_text(colour="black", size = 18),
+        axis.title.x = element_text(size = 23, margin=margin(15,0,0,0)),
+        axis.title.y = element_text(size = 23, margin=margin(0,15,0,0)))
+
+stability_time_by_metric
+
+png(filename = paste("D:/Ixodes_scapularis_research_2019/tick_dataset_results_analysis/manuscript_figures/figure_7A_stability_time_by_metric ",Sys.Date(),".png", sep = ''), width = 600, height = 454)
+stability_time_by_metric
+dev.off()
+
+###############
+#ticks infected vs proportion wrong before stability
+###############
+
+tested_for_path <- tick_dataset_results[tick_dataset_results$tested_for_b_burgdoferi == "Y",] 
+tested_for_path$label <- "Tested for infection \nof B. burgdorferi"
+
+test_abuance <- tick_dataset_results[tick_dataset_results$tested_for_b_burgdoferi == "N",] 
+test_abuance$label <- "Sampled for \nAbundance"
+
+abundance_vs_infected_ticks <- c(tested_for_path$label, test_abuance$label)
+proportion_wrong_before_stab_for_abudance_vs_infected_ticks <- c(tested_for_path$proportion_wrong_before_stability, test_abuance$proportion_wrong_before_stability)
+
+tick_infection_data <- data.frame(abundance_vs_infected_ticks, proportion_wrong_before_stab_for_abudance_vs_infected_ticks)
+
+t.test(tick_infection_data[tick_infection_data$abundance_vs_infected_ticks == "Tested for infection \nof B. burgdorferi",]$proportion_wrong_before_stab_for_abudance_vs_infected_ticks, tick_infection_data[tick_infection_data$abundance_vs_infected_ticks == "Sampled for \nAbundance",]$proportion_wrong_before_stab_for_abudance_vs_infected_ticks)
+# t = -1.1828, df = 232.98, p-value = 0.2381
+# t-value indicates probability below 0.5
+# low probability of difference between datasets
+#
+# p-value higher than 0.05, accept null hypothesis
+# therefore alternative hypothesis: true difference in means is equal to 0 is supported
+# statiscally insignificant
+# no need for letter assignment
+
+#proportion significant by ticks infected and total ticks
+proportion_wrong_before_stab_by_metric <- ggplot(tick_infection_data, aes(x = abundance_vs_infected_ticks, y = proportion_wrong_before_stab_for_abudance_vs_infected_ticks)) +
+  geom_boxplot() + 
+  geom_jitter() +
+  scale_x_discrete(name = "Sampling metric") +
+  scale_y_continuous(name = "Proportion significantly wrong \nbefore stability", limits = c(0, 1.05)) +
+  theme(axis.line.x = element_line(size = 0.5, colour = "black"),
+        axis.line.y = element_line(size = 0.5, colour = "black"),
+        axis.line = element_line(size=1, colour = "black"),
+        panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(),
+        panel.border = element_blank(),
+        panel.background = element_blank(),
+        plot.title=element_text(size = 27, margin=margin(0,0,15,0)),
+        axis.text.x=element_text(colour="black", size = 18),
+        axis.text.y=element_text(colour="black", size = 18),
+        axis.title.x = element_text(size = 23, margin=margin(15,0,0,0)),
+        axis.title.y = element_text(size = 23, margin=margin(0,15,0,0)))
+
+proportion_wrong_before_stab_by_metric
+
+png(filename = paste("D:/Ixodes_scapularis_research_2019/tick_dataset_results_analysis/manuscript_figures/figure_7B_proportion_wrong_before_stab_by_metric ",Sys.Date(),".png", sep = ''), width = 600, height = 454)
+proportion_wrong_before_stab_by_metric
+dev.off()
+
+###############
+# Combined plots 7A and 7B
+###############
+
+#arrange plots 7A and 7B into single image
+figure7AB <- ggarrange(
+  stability_time_by_metric + 
+    scale_x_discrete(name = NULL) + 
+    theme(axis.title.y = element_text(margin=margin(0,-20,0,0))), 
+  proportion_wrong_before_stab_by_metric, 
+  labels = c("A", "B"), nrow = 2, ncol=1, align = "v", font.label = list(size=25), hjust=-7
+)
+
+figure7AB
+
+png(filename = paste("D:/Ixodes_scapularis_research_2019/tick_dataset_results_analysis/manuscript_figures/figure_7AB ",Sys.Date(),".png", sep = ''), width = 600, height = 908)
+figure7AB
+dev.off()
